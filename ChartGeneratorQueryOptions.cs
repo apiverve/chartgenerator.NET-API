7406980f7131710846e01a63336d188ab5a010c0
APIVerve Team <hello@apiverve.com>
2025-02-21 00:46:50 -0600

    Added new API Endpoints

---
+++ ChartGeneratorQueryOptions.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace APIVerve
+{
+    public class ChartGeneratorQueryOptions {
+public string type { get; set; }
+public array data[labels] { get; set; }
+public string data[datasets][0][label] { get; set; }
+public array data[datasets][0][data] { get; set; }
+public string data[datasets][1][label] { get; set; }
+public array data[datasets][1][data] { get; set; }
+}
+
+}
