d3d0cf61f2f33cc7b4d4d9eb2c85d7272bd7af15
APIVerve Team <hello@apiverve.com>
2025-02-21 00:46:53 -0600

    Added new API Endpoints

---
+++ ResponseObj.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace APIVerve
+{
+public class data
+{
+    [JsonProperty("id")]
+    public string id { get; set; }
+
+    [JsonProperty("format")]
+    public string format { get; set; }
+
+    [JsonProperty("expires")]
+    public int expires { get; set; }
+
+    [JsonProperty("type")]
+    public string type { get; set; }
+
+    [JsonProperty("downloadURL")]
+    public string downloadURL { get; set; }
+
+}
+
+public class ResponseObj
+{
+    [JsonProperty("status")]
+    public string status { get; set; }
+
+    [JsonProperty("error")]
+    public object error { get; set; }
+
+    [JsonProperty("data")]
+    public data data { get; set; }
+
+    [JsonProperty("code")]
+    public int code { get; set; }
+
+}
+
+}
